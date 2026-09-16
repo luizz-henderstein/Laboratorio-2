a0676db865429f108a60f5defd4139c1844cd968
Luis Henderson <luis.henderson@estudiantes.utec.edu.uy>
2026-09-16 15:45:11 -0300

    Configurar puertos para los ocho LEDs

--- Parte D/Receptor/Receptor/main.asm
+++ Parte D/Receptor/Receptor/main.asm
@@ -9,6 +9,13 @@ inicio:
     out SPH, r16
     ldi r16, LOW(RAMEND)
     out SPL, r16
+    clr r16
+    out PORTB, r16
+    out PORTC, r16
+    ldi r16, 0b00111111
+    out DDRB, r16
+    ldi r16, 0b00000011
+    out DDRC, r16
 
 principal:
     rjmp principal
