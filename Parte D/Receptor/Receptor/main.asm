4cc2e6c69f003f25c3e8f8fe75e5951f13402d3f
Luis Henderson <luis.henderson@estudiantes.utec.edu.uy>
2026-09-16 15:45:28 -0300

    Configurar recepcion UART a 9600 baudios

--- Parte D/Receptor/Receptor/main.asm
+++ Parte D/Receptor/Receptor/main.asm
@@ -1,4 +1,7 @@
 .include "m328pdef.inc"
+.equ F_CPU = 16000000
+.equ BAUD = 9600
+.equ BPS = (F_CPU/16/BAUD)-1
 
 .cseg
 .org 0x0000
@@ -16,6 +19,20 @@ inicio:
     out DDRB, r16
     ldi r16, 0b00000011
     out DDRC, r16
+    ldi r16, LOW(BPS)
+    ldi r17, HIGH(BPS)
+    rcall initUART
 
 principal:
     rjmp principal
+
+initUART:
+    sts UBRR0L, r16
+    sts UBRR0H, r17
+    clr r16
+    sts UCSR0A, r16
+    ldi r16, (1<<RXEN0)
+    sts UCSR0B, r16
+    ldi r16, (1<<UCSZ01)|(1<<UCSZ00)
+    sts UCSR0C, r16
+    ret
