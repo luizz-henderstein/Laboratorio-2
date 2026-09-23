be98317620732c544b22c29efd14eab5ce0cf9ea
ariel-demarco <229033163+ariel-demarco@users.noreply.github.com>
2026-09-23 20:33:01 -0300

    Configurar salida PWM auxiliar

--- Parte B/DAC_R2R/DAC_R2R/main.asm
+++ Parte B/DAC_R2R/DAC_R2R/main.asm
@@ -17,9 +17,18 @@ inicio:
     out DDRB, temporal
     ldi temporal, 0b00000011
     out DDRC, temporal
+    ldi temporal, (1<<DDD3)
+    out DDRD, temporal
     clr temporal
     out PORTB, temporal
     out PORTC, temporal
+
+    ldi temporal, (1<<COM2B1)|(1<<WGM21)|(1<<WGM20)
+    sts TCCR2A, temporal
+    ldi temporal, (1<<CS20)
+    sts TCCR2B, temporal
+    clr temporal
+    sts OCR2B, temporal
 
 principal:
     rjmp principal
